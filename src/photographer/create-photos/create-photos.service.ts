beb679534595b0028b074a0e9cae55173b1d6811
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-21 10:18:43 -0600

    📅  | Sat May 21 10:18:43 MDT 2022

--- src/photographer/create-photos/create-photos.service.ts
+++ src/photographer/create-photos/create-photos.service.ts
@@ -1,5 +1,15 @@
 import * as puppeteer from 'puppeteer';
 import { Injectable } from '@nestjs/common';
+const I_PHONE_13_PRO_MAX_LANDSCAPE = {
+  key: 'iPhone 13 Pro Max landscape',
+  fileName: 'i-phone-13-pro-max-landscape',
+};
+const I_PHONE_13 = { key: 'iPhone 13', fileName: 'i-phone-13' };
+const I_PHONE_13_LANDSCAPE = {
+  key: 'iPhone 13 landscape',
+  fileName: 'i-phone-13-landscape',
+};
+type ChosenDevice = { key: string; fileName: string };
 
 @Injectable()
 export class CreatePhotosService {
@@ -8,10 +18,15 @@ export class CreatePhotosService {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
       await page.goto('https://example.com');
-      await page.screenshot({
-        path: './photographer_photos/screenshot-new.png',
-      });
+      await this.takeShot(I_PHONE_13, page);
       await browser.close();
     })();
+  }
+  async takeShot(device: ChosenDevice, page: puppeteer.Page) {
+    const deviceOptions = puppeteer.devices[device.key];
+    await page.emulate(deviceOptions);
+    await page.screenshot({
+      path: `./photographer_photos/${device.fileName}.png`,
+    });
   }
 }
