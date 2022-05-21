1154be0dfbf84c12fc7fc14ea204666dc19a47bf
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-21 13:46:40 -0600

    📅  | Sat May 21 13:46:39 MDT 2022

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhotographerModule } from './photographer/photographer.module';
+import { join } from 'path';
 
 @Module({
   imports: [PhotographerModule],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const PORT = process.env.PORT || 3000;
+  app.enableCors();
+  const pathToPhotos = join(__dirname, '..', 'photographer_photos');
+  console.log(pathToPhotos);
+  app.useStaticAssets(pathToPhotos);
+  await app.listen(PORT);
+  console.log(PORT);
 }
 bootstrap();
