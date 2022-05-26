7c148108c626848c810446b0b27229c3c189cac2
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-25 20:47:13 -0600

    📅  | Wed May 25 20:47:13 MDT 2022

--- src/photographer/create-photos/create-photos.service.ts
+++ src/photographer/create-photos/create-photos.service.ts
@@ -1,80 +1,31 @@
+import { devices } from './devices';
 import * as puppeteer from 'puppeteer';
 import { HttpException, Injectable } from '@nestjs/common';
-const I_PAD_PRO_11 = {
-  key: 'iPad Pro 11',
-  fileName: 'i-pad-pro-11',
-};
-const I_PAD_PRO_11_LANDSCAPE = {
-  key: 'iPad Pro 11 landscape',
-  fileName: 'i-pad-pro-11-landscape',
-};
-const I_PAD_GEN_7 = {
-  key: 'iPad (gen 7)',
-  fileName: 'i-pad-gen-7',
-};
-const I_PAD_GEN_7_LANDSCAPE = {
-  key: 'iPad (gen 7) landscape',
-  fileName: 'i-pad-gen-7-landscape',
-};
-const I_PAD_MINI_LANDSCAPE = {
-  key: 'iPad Mini landscape',
-  fileName: 'i-pad-mini-landscape',
-};
-const I_PAD_MINI = {
-  key: 'iPad Mini',
-  fileName: 'i-pad-mini',
-};
-const I_PHONE_SE_LANDSCAPE = {
-  key: 'iPhone SE landscape',
-  fileName: 'i-phone-se-landscape',
-};
-const I_PHONE_SE = {
-  key: 'iPhone SE',
-  fileName: 'i-phone-se',
-};
-const I_PHONE_13_MINI = {
-  key: 'iPhone 13 Mini',
-  fileName: 'i-phone-13-mini',
-};
-const I_PHONE_13_MINI_LANDSCAPE = {
-  key: 'iPhone 13 Mini landscape',
-  fileName: 'i-phone-13-mini-landscape',
-};
-const I_PHONE_13_PRO_MAX = {
-  key: 'iPhone 13 Pro Max',
-  fileName: 'i-phone-13-pro-max',
-};
-const I_PHONE_13_PRO_MAX_LANDSCAPE = {
-  key: 'iPhone 13 Pro Max landscape',
-  fileName: 'i-phone-13-pro-max-landscape',
-};
-const I_PHONE_13 = { key: 'iPhone 13', fileName: 'i-phone-13' };
-const I_PHONE_13_LANDSCAPE = {
-  key: 'iPhone 13 landscape',
-  fileName: 'i-phone-13-landscape',
-};
-type ChosenDevice = { key: string; fileName: string };
+import { ChosenDevice } from './devices';
 
 @Injectable()
 export class CreatePhotosService {
   async takePhotos(url: string, elementSelector: string) {
     await (async () => {
+      console.log('AM I WORKING');
+      const browser = await puppeteer.launch({
+        args: ['--disable-dev-shm-usage'],
+      });
       try {
         const browser = await puppeteer.launch({
           args: ['--disable-dev-shm-usage'],
         });
         const page = await browser.newPage();
         await page.goto(url, { waitUntil: 'networkidle0' });
-        console.log('HELLO BEFORE SCROLL');
-        if (element) {
-          await page.waitForSelector(elementSelector);
-          await page.$eval(elementSelector, (targetElement) => {
-            if (targetElement) {
-              console.log('HELLO FROM SCROLL');
-              targetElement.scrollIntoView();
-            }
-          });
+        for (
+          let deviceIndex = 0;
+          deviceIndex < devices.length;
+          deviceIndex += 1
+        ) {
+          console.log('for loop');
+          await this.takeShot(devices[deviceIndex], page);
         }
+        /*
         await this.takeShot(I_PAD_PRO_11_LANDSCAPE, page);
         await this.takeShot(I_PAD_PRO_11, page);
         await this.takeShot(I_PAD_GEN_7_LANDSCAPE, page);
@@ -91,49 +42,76 @@ export class CreatePhotosService {
         await this.takeShot(I_PHONE_13_PRO_MAX, page);
         await this.takeShot(I_PHONE_13_LANDSCAPE, page);
         await this.takeShot(I_PHONE_13, page);
-
+        */
+        console.log('before first desktop');
         await page.emulate({
           viewport: { width: 1920, height: 1080 },
           userAgent: 'Desktop Computer 2',
         });
+        //await page.waitForNetworkIdle();
+
+        await this.scrollToElement(page, 'footer');
 
         await page.screenshot({
           path: `./photographer_photos/1929x1080.png`,
         });
 
+        console.log('before second deskto');
         await page.emulate({
           viewport: { width: 1366, height: 768 },
           userAgent: 'Desktop Computer',
         });
+        console.log('after emulate first desktip before scroll');
+        //await page.waitForNetworkIdle();
+        await this.scrollToElement(page, 'footer');
+        console.log('after first desktop scro');
 
         await page.screenshot({
           path: `./photographer_photos/1366x768.png`,
         });
-        await browser.close();
         console.log('puppeteer finished');
       } catch (error) {
         console.log('PUPPETEER ERROR');
         console.error(error);
         throw new HttpException(error.message, 400);
       }
+      await browser.close();
     })();
     return { status: 'photoshoot finished' };
   }
   async scrollToElement(page: puppeteer.Page, elementSelector: string) {
     try {
-      await page.$(elementSelector);
-      await page.$eval(elementSelector, (selectedElement) => {
-        selectedElement.scrollIntoView();
-      });
+      console.log(`scroll to element: ${elementSelector}`);
+      try {
+        await page.waitForTimeout(200);
+        console.log('1');
+        const selectedElement = await page.$('footer');
+        console.log('2');
+        await page.evaluate(
+          (selectedElement) => selectedElement.scrollIntoView(),
+          selectedElement,
+        );
+        console.log('3');
+        await page.waitForTimeout(200);
+      } catch (error) {
+        console.error('selector error');
+        console.error(error, 'actuall error log');
+      }
     } catch (error) {
-      console.error('selector error');
+      console.error(error);
     }
   }
   async takeShot(device: ChosenDevice, page: puppeteer.Page) {
     const deviceOptions = puppeteer.devices[device.key];
     await page.emulate(deviceOptions);
+    await page.waitForNetworkIdle();
+    await page.waitForSelector('footer');
+    console.log('before scroll');
+    await this.scrollToElement(page, 'footer');
+    console.log('after scroll before screen shot');
     await page.screenshot({
       path: `./photographer_photos/${device.fileName}.png`,
     });
+    console.log('after screen shot');
   }
 }
---
+++ src/photographer/create-photos/devices.ts
@@ -0,0 +1,70 @@
+export type ChosenDevice = { key: string; fileName: string };
+const I_PAD_PRO_11 = {
+  key: 'iPad Pro 11',
+  fileName: 'i-pad-pro-11',
+};
+const I_PAD_PRO_11_LANDSCAPE = {
+  key: 'iPad Pro 11 landscape',
+  fileName: 'i-pad-pro-11-landscape',
+};
+const I_PAD_GEN_7 = {
+  key: 'iPad (gen 7)',
+  fileName: 'i-pad-gen-7',
+};
+const I_PAD_GEN_7_LANDSCAPE = {
+  key: 'iPad (gen 7) landscape',
+  fileName: 'i-pad-gen-7-landscape',
+};
+const I_PAD_MINI_LANDSCAPE = {
+  key: 'iPad Mini landscape',
+  fileName: 'i-pad-mini-landscape',
+};
+const I_PAD_MINI = {
+  key: 'iPad Mini',
+  fileName: 'i-pad-mini',
+};
+const I_PHONE_SE_LANDSCAPE = {
+  key: 'iPhone SE landscape',
+  fileName: 'i-phone-se-landscape',
+};
+const I_PHONE_SE = {
+  key: 'iPhone SE',
+  fileName: 'i-phone-se',
+};
+const I_PHONE_13_MINI = {
+  key: 'iPhone 13 Mini',
+  fileName: 'i-phone-13-mini',
+};
+const I_PHONE_13_MINI_LANDSCAPE = {
+  key: 'iPhone 13 Mini landscape',
+  fileName: 'i-phone-13-mini-landscape',
+};
+const I_PHONE_13_PRO_MAX = {
+  key: 'iPhone 13 Pro Max',
+  fileName: 'i-phone-13-pro-max',
+};
+const I_PHONE_13_PRO_MAX_LANDSCAPE = {
+  key: 'iPhone 13 Pro Max landscape',
+  fileName: 'i-phone-13-pro-max-landscape',
+};
+const I_PHONE_13 = { key: 'iPhone 13', fileName: 'i-phone-13' };
+const I_PHONE_13_LANDSCAPE = {
+  key: 'iPhone 13 landscape',
+  fileName: 'i-phone-13-landscape',
+};
+export const devices = [
+  I_PAD_MINI,
+  I_PAD_MINI_LANDSCAPE,
+  I_PAD_GEN_7,
+  I_PAD_GEN_7_LANDSCAPE,
+  I_PAD_PRO_11,
+  I_PAD_PRO_11_LANDSCAPE,
+  I_PHONE_13,
+  I_PHONE_13_LANDSCAPE,
+  I_PHONE_SE,
+  I_PHONE_SE_LANDSCAPE,
+  I_PHONE_13_MINI,
+  I_PHONE_13_MINI_LANDSCAPE,
+  I_PHONE_13_PRO_MAX,
+  I_PHONE_13_PRO_MAX_LANDSCAPE,
+];
